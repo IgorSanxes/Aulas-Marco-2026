fd6b8e341d8c02370be0445c22b99ba5d0838a00
IgorSanxes <igor_sanxes@hotmail.com>
2026-03-12 20:37:28 -0300

    Create repeticao-for.py

---
+++ repeticao-for.py
@@ -0,0 +1,11 @@
+alunos = ["Michelle", "Lorenzo", "Santella", "Figli"]
+
+for aluno in alunos:
+    print(f"Nome do Aluno: {aluno}")
+
+#contador = 0
+#tamanho = len(alunos)
+
+#while contador <=tamanho:
+#    print(f"Nome do Aluno: {alunos[contador]}")
+#    contador += 1
