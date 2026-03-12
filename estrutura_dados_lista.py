014e796936d5e50635854f15b84ce09abbca8e2b
IgorSanxes <igor_sanxes@hotmail.com>
2026-03-12 20:37:19 -0300

    Create estrutura_dados_lista.py

---
+++ estrutura_dados_lista.py
@@ -0,0 +1,20 @@
+alunos = ["Michelle", "Lorenzo", "Santella", "Figli"]
+
+print (alunos)
+
+alunos.append('Jair')
+alunos.append('Igor')
+
+print (alunos)
+
+aluno_removido = alunos.pop(1)
+
+print (alunos)
+
+alunos[0] = 'Michelle Sales'
+
+print (alunos)
+
+print (alunos.__len__())
+
+print (aluno_removido)
